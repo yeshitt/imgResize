9804aeced602210302c90895596ccaca906e4e6f
yeshitt <94923689+yeshitt@users.noreply.github.com>
2023-01-21 13:54:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ downloadBtn = document.querySelector(".download-btn");
 let ogImageRatio;
 
 const loadFile = (e) => {
-    const file = e.target.files[0]; // getting first user selected file
-    if(!file) return; // return if user hasn't selected any file
-    previewImg.src = URL.createObjectURL(file); // passing selected file url to preview img src
-    previewImg.addEventListener("load", () => { // once img loaded
+    const file = e.target.files[0]; 
+    if(!file) return; 
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => { 
         widthInput.value = previewImg.naturalWidth;
         heightInput.value = previewImg.naturalHeight;
         ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
@@ -22,13 +22,11 @@ const loadFile = (e) => {
 }
 
 widthInput.addEventListener("keyup", () => {
-    // getting height according to the ratio checkbox status
     const height = ratioInput.checked ? widthInput.value / ogImageRatio : heightInput.value;
     heightInput.value = Math.floor(height);
 });
 
 heightInput.addEventListener("keyup", () => {
-    // getting width according to the ratio checkbox status
     const width = ratioInput.checked ? heightInput.value * ogImageRatio : widthInput.value;
     widthInput.value = Math.floor(width);
 });
@@ -38,21 +36,15 @@ const resizeAndDownload = () => {
     const a = document.createElement("a");
     const ctx = canvas.getContext("2d");
 
-    // if quality checkbox is checked, pass 0.5 to imgQuality else pass 1.0
-    // 1.0 is 100% quality where 0.5 is 50% of total. you can pass from 0.1 - 1.0
     const imgQuality = qualityInput.checked ? 0.5 : 1.0;
-
-    // setting canvas height & width according to the input values
     canvas.width = widthInput.value;
     canvas.height = heightInput.value;
 
-    // drawing user selected image onto the canvas
     ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
     
-    // passing canvas data url as href value of <a> element
     a.href = canvas.toDataURL("image/jpeg", imgQuality);
-    a.download = new Date().getTime(); // passing current time as download value
-    a.click(); // clicking <a> element so the file download
+    a.download = new Date().getTime(); 
+    a.click();
 }
 
 downloadBtn.addEventListener("click", resizeAndDownload);
